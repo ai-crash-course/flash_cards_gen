cb6d860bc721b6b1dbc39e6005f23a0f3c0577a1
Toolkit User <issam.laradji@gmail.com>
2023-12-24 21:18:21 +0000

    added gradio

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import os
-import configs
-
 import argparse
 import json
-import random
 
 import gradio as gr
 import numpy as np
@@ -38,36 +35,38 @@ def main(model_name, txt_path, reset=False):
     else:
         flash_card_list = json.load(open(path, "r"))
 
-    # Gradio
-
-    # Clean up the flash card list by rremoving anything preceding ':'
+    # Clean up the flash card list by removing anything preceding ':'
     for i in range(len(flash_card_list)):
         for key in flash_card_list[i].keys():
             if ":" in flash_card_list[i][key]:
                 flash_card_list[i][key] = flash_card_list[i][key].split(":")[1].strip()
 
+    # Get question and answer list and shuffle it
     index = 0
     answer_keys = ["true_answer", "wrong_answer_1", "wrong_answer_2", "wrong_answer_3"]
-    # random permutations of size 4
+
     indices = np.random.permutation(4)
+    question = flash_card_list[index]["question"]
 
     answer_list = np.array([flash_card_list[index][a] for a in answer_keys])
     answer_id = np.where(indices == 0)[0][0]
-    # randomly shuffle answer_list and return
+
     answer_list = list(answer_list[indices])
 
-    def greet(name):
+    def submit_answer(name):
         if name == answer_list[answer_id]:
             return gr.update(value="Correct", visible=True)
         else:
             return gr.update(value="Wrong", visible=True)
 
-    question = flash_card_list[index]["question"]
+    # Create blocks of UI elements
     with gr.Blocks() as demo:
         radio = gr.Radio(answer_list, value=answer_list[0], label=question)
         label = gr.Label(visible=False)
-        greet_btn = gr.Button("Greet")
-        greet_btn.click(fn=greet, inputs=radio, outputs=label, api_name="Submit")
+        greet_btn = gr.Button("Submit Answer")
+        greet_btn.click(
+            fn=submit_answer, inputs=radio, outputs=label, api_name="Submit"
+        )
 
     demo.launch()
     print("Experiment Completed")
